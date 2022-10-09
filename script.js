00477e1ea46feee12de5fe00ccd2f9f09aa4f086
Vitória Chaves <69561315+mendesv1t@users.noreply.github.com>
2022-10-09 19:56:55 -0300

    adicionando botoes de remover ultimo circulo e reinicializar jogo

--- script.js
+++ script.js
@@ -4,33 +4,67 @@ titulo.style.justifyContent = "center";
 titulo.style.display = "grid";
 titulo.id = 'titulo';
 
-let image = document.createElement('img');
-image.src = 'assets/restart.png';   
-image.style.width = '30px';
-image.style.height = '30px';
-image.title = "Reset";
+let divButtons = document.createElement("div");
+divButtons.style.display = "flex";
+divButtons.style.justifySelf = 'center';
 
-var button = document.createElement('button');
-button.type = 'button';
-button.className = 'btn-styled';
-button.appendChild(image);
-button.style.borderRadius = '20%';
-button.style.justifySelf = "center";
+let imageRestart = document.createElement('img');
+imageRestart.src = 'assets/restart.png';   
+imageRestart.style.width = '30px';
+imageRestart.style.height = '30px';
+imageRestart.title = "Restart";
+imageRestart.className = 'tooltipClass';
+
+var restart = document.createElement('button');
+restart.type = 'button';
+restart.appendChild(imageRestart);
+restart.style.borderRadius = '20%';
+restart.style.display = 'inline';
+restart.style.margin = '10px';
+
+let imageAddCircle = document.createElement('img');
+imageAddCircle.src = 'assets/addCircle.png';   
+imageAddCircle.style.width = '30px';
+imageAddCircle.style.height = '30px';
+imageAddCircle.title = "Add Circle";
+imageAddCircle.className = 'tooltipClass';
+
+var addCircle = document.createElement('button');
+addCircle.type = 'button';
+addCircle.appendChild(imageAddCircle);
+addCircle.style.borderRadius = '20%';
+addCircle.style.display = 'inline';
+addCircle.style.margin = '10px';
+
+let imageRemoveCircle = document.createElement('img');
+imageRemoveCircle.src = 'assets/removeCircle.png';   
+imageRemoveCircle.style.width = '30px';
+imageRemoveCircle.style.height = '30px';
+imageRemoveCircle.title = "Remove last Circle";
+imageRemoveCircle.className = 'tooltipClass';
+
+var removeCircle = document.createElement('button');
+removeCircle.type = 'button';
+removeCircle.appendChild(imageRemoveCircle);
+removeCircle.style.borderRadius = '20%';
+removeCircle.style.display = 'inline';
+removeCircle.style.margin = '10px';
+
+divButtons.appendChild(addCircle);
+divButtons.appendChild(restart);
+divButtons.appendChild(removeCircle);
 
 
 let colorsList = ['#008000','#f2bc1a','#305fed','#f2331a','#a525b0'];
-
 let circles = document.createElement("div");
+circles.id = 'circles';
 circles.className = "circles-container";
-
-let size = 78;
-
 let circlesIdList = [];
 
-for (let i = 0; i < size; i++) {
+addCircle.onclick = function() {
     let circle = document.createElement("div");
     circle.className = "circle";
-    circle.id = "circle" + i;
+    circle.id = "circle";
     let color = colorsList[Math.floor(Math.random()*5)];
     circle.style.backgroundColor = color;
     circle.onclick = function() {
@@ -38,21 +72,26 @@ for (let i = 0; i < size; i++) {
     }
     circlesIdList.push({id: circle.id, color: color});
     circles.appendChild(circle);
-
+    
 }
 
-button.onclick = function() {
-    for (let j = 0; j < circlesIdList.length; j++) {
-        let circle = document.getElementById(circlesIdList[j]['id']);
-        let color = circlesIdList[j]['color'];
-        circle.style.backgroundColor = color;
+restart.onclick = function() {
+    let circles = document.getElementById('circles');
+    while (circles.firstChild) {
+        circles.removeChild(circles.lastChild);  
+        
     }
+      
 };
+
+removeCircle.onclick = function() {
+    circles.removeChild(circles.lastChild);
+}
  
 document.addEventListener('DOMContentLoaded', function(event) {
     document.body.style.display = "grid";
     document.body.appendChild(titulo);
-    document.body.appendChild(button);
+    document.body.appendChild(divButtons);
     document.body.appendChild(circles);
 });
 
